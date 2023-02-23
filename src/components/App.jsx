6f60084666a0aced94f9092b12edbd5123e020b1
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-02-23 12:39:56 +0200

    finnaly fix coment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
-// import {  Routes, Route } from "react-router-dom";
-import Header from "./Header/Header"; 
-// import Home from "../Pages/Home/Home";
-// import Movies from "../Pages/Movies/Movies";
-// import MovieDetails from "../Pages/MovieDetails/MovieDetails"
-// import Cast from 'components/Cast/Cast'
-// import Reviews from "components/Reviews/Reviews";
 
+import Header from "./Header/Header"; 
 import AppRoutes from 'components/AppRoutes/AppRoutes'
 
 export const App = () => {
@@ -14,17 +8,7 @@ export const App = () => {
      <Header/>
      <AppRoutes/>
      </>
-   //   <Routes>
-    //  <Route path="/" element={<Home />} ></Route>
-     //   <Route path="/" element={<Home />} />
-     //   <Route path="/movies" element={<Movies/>} />
-      //  <Route path='/movies/:id' element={<MovieDetails/>}>
-      //  <Route path="cast" element={<Cast />} />
-      //  <Route path="reviews" element={<Reviews />} /> 
-       //   </Route> 
-       //  <Route path="*" element={<Home/>} />
-      // </Routes> 
-     
+    
   )
 };
 
